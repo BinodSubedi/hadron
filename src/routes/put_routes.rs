446738f4823e9907b6db466456d6ded2fc8693a5
Binodsubedi <binodsubedi125@gmail.com>
2023-10-28 15:44:18 +0545

    Json input and Json response structure created

--- src/routes/put_routes.rs
+++ src/routes/put_routes.rs
@@ -1,20 +1,50 @@
-use rocket::{serde::{Serialize,Deserialize}, form::Form};
+use rocket::{serde::{Serialize,Deserialize, json::Json}, form::Form};
 
 
 #[derive(Debug,Clone,FromForm,Serialize,Deserialize)]
 #[serde(crate = "rocket::serde")]
-pub struct StandardInputFormat<'a>{
-    Name: &'a str
+pub struct PutStandardInputFormat<'a>{
+    data: &'a str,
 }
 
 
-#[put("/<collection>", data="<form>")]
-pub async fn put_one(collection:String, form:Form<StandardInputFormat<'_>>)-> String{
-    println!("{:?}",form.into_inner().Name);
+#[derive(Debug,Clone,Serialize,Deserialize)]
+#[serde(crate = "rocket::serde")]
+enum ResponseStatus{
+    Success,
+    Failed
+}
+
+#[derive(Debug,Clone,Deserialize,Serialize)]
+#[serde(crate="rocket::serde")]
+pub struct PutStandardResponse{
+    
+    status: u8,
+    response: ResponseStatus
+
+
+}
+
+
+#[put("/<collection>",format = "json",data="<body>")]
+pub async fn put_one(collection:String, body:Json<PutStandardInputFormat<'_>>)-> Json<PutStandardResponse>{
+//pub async fn put_one(collection:String, form:Form<PutStandardInputFormat<'_>>)-> Json<PutStandardResponse>{
+    //println!("{:?}",form);
+     
+    
 
 //    println!("{:?}", data);
         
     //result
-    String::from("Sent")
+    //String::from("Sent")
+    //
+
+    let res = PutStandardResponse{
+        status: 200,
+        response: ResponseStatus::Success
+
+    };
+
+    Json(res)
 
 }
